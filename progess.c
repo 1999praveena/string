1161e284436e446d7c1a14472d2fef0af501268f
Praveena A M <praveenaarokiaraj@gmail.com>
2019-02-01 12:31:00 +0530

    Create progess.c

---
+++ progess.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdio.h>
+void main()
+{
+int a,b,c,d;
+printf("enter first n,common differnce,n number");
+scanf("%d%d%d",&a,&b,&c);
+d=n*(2*a+(n-1)*d)/2;
+printf(d);
+getch();
+}
